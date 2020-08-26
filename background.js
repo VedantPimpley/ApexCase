462323362b1104c4471ba9843e2c45d26a2a0877
VedantPimpley <vedantpimpley7@gmail.com>
2020-08-26 12:12:22 +0530

    Created submenu, renamed menu ids

--- background.js
+++ background.js
@@ -2,15 +2,51 @@ console.log("from background");
 
 //create context menu in UI
 chrome.contextMenus.create({
-  id: "capitalizer",
+  id: "parentMenu",
   title: "Capitalizer",
   type: 'normal',
   contexts: ['editable'],
-  //by using 'editable' and not 'selection' here, the contextMenu only appears in editable elements such as a textfield
+  //note that we use editable, not selection
+});
+
+//capitalize everything sub-contextMenu
+chrome.contextMenus.create({
+  id: "capitalizeAll",
+  parentId: "parentMenu",
+  title: "Capitalize entire selection",
+  type: 'normal',
+  contexts: ['editable'],
+});
+
+//undo sub-contextMenu
+chrome.contextMenus.create({
+  id: "undo",
+  parentId: "parentMenu",
+  title: "Undo selected",
+  type: 'normal',
+  contexts: ['editable'],
+});
+
+//per-word capitalize sub-contextMenu
+chrome.contextMenus.create({
+  id: "perWord",
+  parentId: "parentMenu",
+  title: "Capitalize each word",
+  type: 'normal',
+  contexts: ['editable'],
+});
+
+////per-sentence capitalize sub-contextMenu
+chrome.contextMenus.create({
+  id: "perSentence",
+  parentId: "parentMenu",
+  title: "Capitalize first word of each sentence",
+  type: 'normal',
+  contexts: ['editable'],
 });
 
 chrome.contextMenus.onClicked.addListener(e => {
-  if(e.selectionText && e.menuItemId === "capitalizer") {
+  if(e.selectionText && e.menuItemId === "capitalizeAll") {
     chrome.tabs.executeScript(null, {
       file: "./content.js"
     });
